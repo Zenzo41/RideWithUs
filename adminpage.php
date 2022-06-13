ce88dae22e24f8ac85f9a70b39489af940f03fb5
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2022-06-13 21:10:54 +0545

    Add files via upload

---
+++ adminpage.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <title>ADMIN</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <style>
+        h1,body{
+            color:azure;
+            text-align:center;
+            font-size:50px;
+            padding:20%;
+            font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+        }
+        body{
+            background-color:black;
+        }
+        input{
+            color:black;
+            font-size:x-large;
+        }
+    </style></head>
+    <body>
+    
+        <h1>Welcome Admin</h1>
+
+        <div class="btn-group btn-group-toggle" data-toggle="buttons">
+  <label class="btn btn-secondary active">
+ <input type="submit" value="Upload"> Upload Product </label></a>
+  
+  <label class="btn btn-secondary">
+  <input type="submit" value="Database">User Database</label></a>
+  
+    </body>
+</html>
